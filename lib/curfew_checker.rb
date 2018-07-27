cf81937a0aa12b3ef988629e563f17f86a49de3b
ammyhran <ammyhran@gmail.com>
2018-07-27 16:47:48 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,14 +1,16 @@
 def simple_curfew_checker(time)
   if time > 11
     puts "You did not make curfew!"
-  elsif time <= 11
-    puts "You made curfew!"
   end
 end
 
 def curfew_checker(time)
-  
+  if time > 11
+    puts "You did not make curfew!"
+  elsif
+    puts "You made curfew!"
 end
+end 
 
 def complex_curfew_checker(time)
   # code goes here
